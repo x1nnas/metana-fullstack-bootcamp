d8ba9234e41c7803bb8809916dd437637e379786
Jose Ferreira <jmiguelf2000@gmail.com>
2025-02-08 19:29:50 +0400

    Initial commit

---
+++ Module 7 Assignment/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const nodemon = require('nodemon');
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle 404
+app.use((req, res) => {
+    res.status(404).send('404: Page not found');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
